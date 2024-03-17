bff426638a35709960673711e4abb60bdfe88e83
salto1337 <154664492+salto1337@users.noreply.github.com>
2024-03-17 14:30:25 +0100

    Update Contact.jsx

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -5,8 +5,8 @@ export function Contact() {
 		console.log("FORMULARZ WYSLANY!");
 	}
 	return (
-		<div>
-			<h1 className="text-center font-bold text-5xl mb-[200px] uppercase">
+		<div className="mt-[120px] lg:mt-[5gi0px]">
+			<h1 className="text-center font-bold text-3xl lg:text-5xl mb-[100px] lg:mb-[150px] uppercase">
 				skontaktuj sie z nami{" "}
 			</h1>
 			<form onSubmit={handleSubmit} className="border-2 p-10">
@@ -16,7 +16,12 @@ export function Contact() {
 					</label>
 					<input className="border-2" type="text" id="name" />
 					<label htmlFor="message">wiadomość</label>
-					<input className="border-2 h-[100px] px-2" type="text" id="message" autoComplete="off" />
+					<input
+						className="border-2 h-[100px] px-2"
+						type="text"
+						id="message"
+						autoComplete="off"
+					/>
 				</div>
 				<div className="flex justify-center mt-10">
 					<button type="submit" className="w-50 border-2 p-2 rounded-2xl">
