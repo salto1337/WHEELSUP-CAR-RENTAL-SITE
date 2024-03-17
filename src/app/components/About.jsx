4076a4663d44f7db5e454cc06e87ea76849be414
salto1337 <154664492+salto1337@users.noreply.github.com>
2024-03-17 14:29:57 +0100

    Update About.jsx

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -5,12 +5,12 @@ import { Questions } from "./Questions";
 export function About() {
 	return (
 		<div className="mt-[100px] mb-[150px] ">
-			<div className="flex flex-col md:flex-row justify-center gap-11 items-center mt-10">
+			<div className="flex flex-col lg:flex-row justify-center gap-11 items-center mt-10">
 				<div>
 					<Image src={mercedesImg} width={550} height={500} alt="auto" />
 				</div>
 				<div>
-					<p className="max-w-[350px]">
+					<p className="lg:max-w-[350px] text-center text-xl lg:text-base">
 						WheelsUp to dynamiczna firma specjalizująca się w wynajmie
 						samochodów, zapewniająca niezrównaną wygodę i elastyczność w
 						podróżach. Nasza szeroka flota pojazdów, obejmująca modele od
